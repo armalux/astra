50bed7278b93893be313c191706cfa66bbff7657
Eric Johnson <eric.johnson@cs2.io>
2015-04-28 19:34:00 -0400

    Updated the setup.py script, getting ready to do second dev release.

--- astra/__init__.py
+++ astra/__init__.py
@@ -1,3 +1,3 @@
 __author__ = 'Eric Johnson'
-__version__ = '0.1a.dev0'
+__version__ = '0.1a.dev1'
 __all__ = ['framework', 'apps', 'user_services', 'team_services', 'test']
--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python3
 
 from distutils.core import setup
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+import astra
 
 setup(
     name='astra',
-    version='0.1a.dev0',
+    version=astra.__version__,
     url='https://github.com/megamandos/astra',
-    description='The Astra Exploit Framework.',
+    description='The Astra Exploit Framework',
     author='Eric Johnson',
     author_email='megamandos@gmail.com',
     packages=[
         'astra',
         'astra.framework',
+        'astra.framework.router',
         'astra.apps',
+        'astra.apps.teamserver',
         'astra.apps.console',
         'astra.apps.console.commands',
-        'astra.user_services',
-        'astra.team_services',
+        'astra.services',
+        'astra.test',
     ],
+    package_data = {
+        'astra.apps.teamserver': ['static/*']
+    },
     requires=[
         'tornado',
         'sqlalchemy'
