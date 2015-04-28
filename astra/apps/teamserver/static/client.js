81b7df2c3b3c8c2b4edeb05ab4d8e88b2a96870e
Eric Johnson <eric.johnson@cs2.io>
2015-04-28 01:02:24 -0400

    Working on implementing WAMP, since there is no python 3 support as of yet.

--- astra/apps/teamserver/static/client.js
+++ astra/apps/teamserver/static/client.js
@@ -1,14 +1,56 @@
 function AstraClient(url) {
+    var client = this;
+
     this.socket = new WebSocket(url);
-    this.socket.onmessage = function(evt) {
-        console.log(JSON.parse(evt.data));
+
+    this.socket.onopen = function() {
+        client.onopen();
+        client.hello('astra.realm');
     };
+
+    this.socket.onmessage = function(evt) {
+        client.onmessage(evt.data);
+
+        var data = JSON.parse(evt.data);
+        console.log(data);
+        switch(data.msg_type) {
+            case 'welcome':
+                client.session_id = data.session_id;
+                break;
+        }
+    };
+
+    this.onopen = function() {};
+    this.onmessage = function(data) {};
+
+    this.next_request_id = 0;
 }
 
-AstraClient.prototype.subscribe = function(channel) {
+AstraClient.prototype.getRequestId = function() {
+    return this.next_request_id++;
+};
+
+AstraClient.prototype.subscribe = function(topic) {
     var msg = {
-        'type': 'subscribe',
-        'channel': channel
+        'msg_type': 'subscribe',
+        'request_id': this.getRequestId(),
+        'options': {},
+        'topic': topic
+    };
+
+    this.socket.send(JSON.stringify(msg));
+};
+
+AstraClient.prototype.hello = function(realm) {
+    var msg = {
+        'msg_type': 'hello',
+        'realm': realm,
+        'details': {
+            'roles': {
+                'publisher': {},
+                'subscriber': {}
+            }
+        }
     };
 
     this.socket.send(JSON.stringify(msg));
@@ -19,6 +61,17 @@ AstraClient.prototype.publish = function(channel, message) {
         'type': 'publish',
         'channel': channel,
         'message': message
+    };
+
+    this.socket.send(JSON.stringify(msg));
+};
+
+AstraClient.prototype.call = function(func_name, args, kwargs) {
+    var msg = {
+        'type': 'call',
+        'function': func_name,
+        'args': args,
+        'kwargs': kwargs
     };
 
     this.socket.send(JSON.stringify(msg));
--- astra/framework/rpc.py
+++ astra/framework/rpc.py
@@ -2,71 +2,105 @@ from tornado import websocket
 import json
 
 
-class JsonMessage:
-    __properties__ = {}
+class ValidationException(Exception):
+    pass
 
-    def __new__(cls, data):
-        obj = object.__new__(cls)
-        for name, value_type in cls.__properties__.items():
-            if name not in data:
-                raise ValueError(name)
 
-            try:
-                value = value_type(data[name])
-            except ValueError:
-                raise ValueError('Bad type: {0}'.format(name))
+def validated_property(prop_type):
+    def function_generator(real_function):
+        def getter(self):
+            return getattr(self, '_' + real_function.__name__)
 
-            setattr(obj, name, value)
+        def setter(self, value):
+            if not isinstance(value, prop_type):
+                raise ValidationException(real_function.__name__)
+
+            real_function(self, value)
+            setattr(self, '_' + real_function.__name__, value)
+
+        return property(getter, setter)
+
+    return function_generator
+
+
+class Handler:
+    _msg_type = None
+    __properties__ = []
+
+    @validated_property(str)
+    def msg_type(self, value):
+        if value != self._msg_type:
+            raise ValidationException('msg_type != {0}'.format(self._msg_type))
+
+    def __new__(cls, *args, **kwargs):
+        obj = super().__new__(cls)
+        for attr_name, attr_value in cls.__dict__.items():
+            if not isinstance(attr_value, property):
+                continue
+
+            obj.__properties__.append(attr_name)
+            setattr(obj, '_' + attr_name, None)
 
         return obj
 
-    def process(self, message):
+    def __init__(self, data):
+        for prop_name in self.__properties__:
+            if prop_name not in data:
+                raise ValidationException('Missing {0}.'.format(prop_name))
+            setattr(self, prop_name, data[prop_name])
+
+    def handle(self, ws):
         raise NotImplementedError()
 
 
-class SubscribeMessage(JsonMessage):
-    __type__ = 'subscribe'
-    __properties__ = {
-        'channel': str
-    }
+class SubscribeHandler(Handler):
+    _msg_type = 'subscribe'
+
+    @validated_property(int)
+    def request_id(self, value):
+        if value > 9007199254740992 or value < 0:
+            raise ValidationException('Request ID out of range.')
+
+    @validated_property(dict)
+    def options(self, value):
+        pass
+
+    @validated_property(str)
+    def topic(self, value):
+        pass
 
 
-class PublishMessage(JsonMessage):
-    __type__ = 'publish'
-    __properties__ = {
-        'channel': str,
-        'message': str
-    }
+class HelloHandler(ServiceUser, Handler):
+    _msg_type = 'hello'
+
+    @validated_property(str)
+    def realm(self, value):
+        pass
+
+    @validated_property(dict)
+    def details(self, value):
+        pass
+
+    def handle(self, ws):
+        if self.realm not in ws._realms:
+            ws.close()
+            return
+
+        session_id = self.services.random.integer(0, 9007199254740992)
 
 
 class MessageHandler(websocket.WebSocketHandler):
-    channels = {}
+    _handlers = {
+        'hello': HelloHandler,
+        'subscribe': SubscribeHandler
+    }
 
-    def publish(self, channel, message):
-        if channel not in self.channels:
-            self.channels[channel] = []
-            return
+    _realms = {
+        'astra.realm': {
+        }
+    }
 
-        msg = json.dumps({
-            'type': 'publish',
-            'channel': channel,
-            'message': message
-        })
-
-        for subscriber in self.channels[channel]:
-            subscriber.write_message(msg)
-
-    def subscribe(self, channel):
-        if channel not in self.channels:
-            self.channels[channel] = []
-
-        if self not in self.channels[channel]:
-            self.channels[channel].append(self)
-
-    def on_close(self):
-        for channel, subscribers in self.channels.items():
-            if self in subscribers:
-                subscribers.remove(self)
+    _sessions = {}
 
     def on_message(self, message):
         try:
@@ -75,19 +109,22 @@ class MessageHandler(websocket.WebSocketHandler):
             self.close()
             return
 
-        if 'type' not in message:
+        if 'msg_type' not in message:
             self.close()
             return
 
-        if message['type'] == 'subscribe':
-            message = SubscribeMessage(message)
-            self.subscribe(message.channel)
-
-        elif message['type'] == 'publish':
-            message = PublishMessage(message)
-            self.publish(message.channel, message.message)
-
-        else:
+        if message['msg_type'] not in self._handlers:
             self.close()
             return
+
+        try:
+            handler = self._handlers[message['msg_type']](message)
+        except ValidationException:
+            self.close()
+            return
+
+        handler.handle(self)
+
+
+
 
