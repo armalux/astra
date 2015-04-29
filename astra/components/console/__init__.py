395c113e70a8a15b246587fd1f63c340174a8415
Eric Johnson <eric.johnson@cs2.io>
2015-04-29 13:32:17 -0400

    Updated the UI and added message timestamps.

--- astra/components/console/__init__.py
+++ astra/components/console/__init__.py
@@ -1,11 +1,13 @@
 from ...framework.router import Component
 import shlex
+import datetime
+
 
 class Console(Component):
     def init(self):
-        print('Console component initialized.')
         self.commands = {}
-        self.commands['hello'] = []
+        self.commands['hello'] = None
+        print('Console component initialized.')
 
     def on_open(self, ws):
         stdin = 'astra.clients.{session_id}.stdin'.format(session_id = ws.session_id)
@@ -14,23 +16,38 @@ class Console(Component):
 
         def cb(message):
             if not len(message['args']):
-                self.publish(stderr, {}, message='Bad event format.')
+                timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                self.publish(stderr, {}, message='Bad event format.', timestamp=timestamp)
                 return
+
+            if not len(message['args'][0]):
+                return
+
+            timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            self.publish(stdout, {}, message=message['args'][0], timestamp=timestamp)
 
             self.on_command(stdout, stderr, message['args'][0])
 
         self.subscribe(stdin, cb)
 
     def on_command(self, stdout, stderr, line):
-        parts = shlex.split(line, posix=True)
+        try:
+            parts = shlex.split(line, posix=True)
+        except ValueError as e:
+            timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            self.publish(stderr, {}, message=str(e), timestamp=timestamp)
+            return
+
         if not len(parts):
             return
 
         if parts[0] not in self.commands:
-            self.publish(stderr, {}, message='No such command: ' + parts[0])
+            timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            self.publish(stderr, {}, message='No such command: ' + parts[0], timestamp=timestamp)
             return
 
-        self.publish(stdout, {}, message='Running command: ' + line)
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.publish(stdout, {}, message='Running command: ' + line, timestamp=timestamp)
 
     def on_close(self, ws):
-        print(ws.session_id)
+        print('{0} disconnected.'.format(ws.session_id))
