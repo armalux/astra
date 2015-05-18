1abc70ac832947d380cad56c853131077f28e9eb
Eric Johnson <eric@cs2.io>
2015-05-17 20:55:41 -0400

    Moved builtin command components to a single module.

--- astra/modules/commands/builtin.py
+++ astra/modules/commands/builtin.py
@@ -1,10 +1,52 @@
 author = 'Eric Johnson'
-uri = 'io.armalux.astra.modules.commands.search'
-name = 'Astra console search command.'
-description = 'Adds the search command to the console.'
+uri = 'modules.commands.builtin'
+name = 'Astra built-in console commands.'
+description = 'Adds the default console commands.'
 
 
 from astra.framework.module import CommandComponent
+import binascii
+
+
+class HelpCommand(CommandComponent):
+    @staticmethod
+    def help(parser):
+        """
+        Display a list of commands and return.
+        """
+
+    def run(self):
+        headers = ('Name', 'Description')
+        rows = [(command.name, command.description) for command in self.console.commands.values()]
+        rows = sorted(rows, key=lambda row: row[0].lower())
+        self.console.print_table(headers, rows)
+
+
+class RandomCommand(CommandComponent):
+    length = None
+
+    @classmethod
+    def help(cls, parser):
+        """
+        Generate random, base64 encoded data, and print it to the command line.
+        """
+        parser.add_argument('length', help='Length of the data to generate.', type=int)
+
+    def run(self):
+        data = self.services.random.bytes(self.length)
+        self.console.print(binascii.b2a_base64(data).decode('ascii').strip())
+
+
+class ReloadCommand(CommandComponent):
+    @classmethod
+    def help(cls, parser):
+        """
+        Reload modules.
+        """
+
+    def run(self):
+        self.services.module.load()
+        self.console.print('[=] Loaded {0} commands.'.format(len(self.console.commands)))
 
 
 class SearchCommand(CommandComponent):
